76be219af9e100f483f3dbc17fda48c001ae7c17
AH <98254846+AO106@users.noreply.github.com>
2022-03-17 18:00:22 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+let elementt = document.getElementById("bo")
+let elementtt = document.getElementById("boo")
+
+
+elementt.addEventListener("click", function(){
+    let ancer = document.getElementById("ancer")
+    ancer.style.color = 'red'
+    let ancerr = document.getElementById("ancerr")
+    ancerr.style.color = 'red'
+    let ancerrr = document.getElementById("ancerrr")
+    ancerrr.style.color = 'red'
+    let ancerrrr = document.getElementById("ancerrrr")
+    ancerrrr.style.color = 'red'
+    let ancerrrrr = document.getElementById("ancerrrrr")
+    ancerrrrr.style.color = 'red'
+    let ancerrrrrr = document.getElementById("ancerrrrrr")
+    ancerrrrrr.style.color = 'red'
+});
+
+elementtt.addEventListener("click", function(){
+    let ancer = document.getElementById("ancer")
+    ancer.style.color = 'blueviolet'
+    let ancerr = document.getElementById("ancerr")
+    ancerr.style.color = 'blueviolet'
+    let ancerrr = document.getElementById("ancerrr")
+    ancerrr.style.color = 'blueviolet'
+    let ancerrrr = document.getElementById("ancerrrr")
+    ancerrrr.style.color = 'blueviolet'
+    let ancerrrrr = document.getElementById("ancerrrrr")
+    ancerrrrr.style.color = 'blueviolet'
+    let ancerrrrrr = document.getElementById("ancerrrrrr")
+    ancerrrrrr.style.color = 'blueviolet'
+});
